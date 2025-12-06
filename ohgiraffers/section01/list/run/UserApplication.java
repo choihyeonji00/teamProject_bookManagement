5e5e2d61a4d2d0c51c1e42f97198f79c13c5f9b0
LimRaynor <morden9137@gmail.com>
2025-12-07 02:52:01 +0900

    마지막 부분 println -> printf 로 변경

--- ohgiraffers/section01/list/run/UserApplication.java
+++ ohgiraffers/section01/list/run/UserApplication.java
@@ -31,13 +31,13 @@ public class UserApplication {
                     System.out.print("PW: ");
                     String pw = sc.nextLine();
 
-                    System.out.print("이름: ");
+                    System.out.print("이름/성함: ");
                     String name = sc.nextLine();
 
                     UserDTO newUser = new UserDTO(id, pw, name);
                     boolean result = userService.register(newUser);
 
-                    System.out.println(result ? "가입 성공" : "이미 존재하는 ID입니다 다른 ID를 사용해 주세요");
+                    System.out.println(result ? "가입 성공 축하축하 " : "아쉽지만 이미 존재하는 ID입니다 다른 ID를 사용해 주세요");
                     break;
 
                 case 2:
@@ -53,18 +53,18 @@ public class UserApplication {
                     break; // ? 는 삼항연산자 boolean -> true false null 세가지일때 쓸수잇는거 나도 프로젝트하면서 배우게됨
 
                 case 3:
-                    System.out.println("=== 사용자 목록 ===");
+                    System.out.println("============ 사용자 목록 =============");
                     for (UserDTO u : userService.getAllUsers()) {
                         System.out.println(u);
                     }
                     break;
 
                 case 9:
-                    System.out.println("프로그램 종료");
+                    System.out.println("===============프로그램 종료===============");
                     return;
 
                 default:
-                    System.out.println("잘못 입력하셨습니다.");
+                    System.out.printf("잘못 입력하셨습니다.");
             }
         }
     }

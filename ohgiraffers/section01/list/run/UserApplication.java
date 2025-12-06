5bf1162b47ab78ec39f416d87524d657b39fa18b
LimRaynor <morden9137@gmail.com>
2025-12-07 00:53:11 +0900

    독립 회원가입 수정

--- ohgiraffers/section01/list/run/UserApplication.java
+++ ohgiraffers/section01/list/run/UserApplication.java
@@ -16,9 +16,9 @@ public class UserApplication {
             System.out.println("===== 사용자 메뉴 =====");
             System.out.println("1. 회원가입");
             System.out.println("2. 로그인");
-            System.out.println("3. 전체 사용자 조회");
+            System.out.println("3. 전체회원조회");
             System.out.println("9. 종료");
-            System.out.print("메뉴 선택: ");
+            System.out.print("메뉴를 골라주세요: ");
 
             int choice = sc.nextInt();
             sc.nextLine();
@@ -37,7 +37,7 @@ public class UserApplication {
                     UserDTO newUser = new UserDTO(id, pw, name);
                     boolean result = userService.register(newUser);
 
-                    System.out.println(result ? "가입 성공!" : "이미 존재하는 ID입니다!");
+                    System.out.println(result ? "가입 성공" : "이미 존재하는 ID입니다 다른 ID를 사용해 주세요");
                     break;
 
                 case 2:
@@ -49,8 +49,8 @@ public class UserApplication {
 
                     UserDTO loginUser = userService.login(loginId, loginPw);
 
-                    System.out.println(loginUser == null ? "로그인 실패!" : "로그인 성공 → " + loginUser);
-                    break;
+                    System.out.println(loginUser == null ? "로그인 실패" : "로그인 성공 → " + loginUser);
+                    break; // ? 는 삼항연산자 boolean -> true false null 세가지일때 쓸수잇는거 나도 프로젝트하면서 배우게됨
 
                 case 3:
                     System.out.println("=== 사용자 목록 ===");
@@ -60,7 +60,7 @@ public class UserApplication {
                     break;
 
                 case 9:
-                    System.out.println("프로그램 종료!");
+                    System.out.println("프로그램 종료");
                     return;
 
                 default:

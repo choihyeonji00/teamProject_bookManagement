c12bccd117061a986515d90690bfe7bb6299eed1
LimRaynor <morden9137@gmail.com>
2025-12-07 04:48:20 +0900

    UserService 에 Map user id 조회 기능 추가

--- ohgiraffers/section01/list/service/UserService.java
+++ ohgiraffers/section01/list/service/UserService.java
@@ -2,11 +2,14 @@ package com.ohgiraffers.section01.list.service;
 
 import com.ohgiraffers.section01.list.dto.UserDTO;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class UserService {
 
     private List<UserDTO> userList = new ArrayList<>();
+private Map<String, UserDTO> userMap = new HashMap<>();// Map를 만들긴 했는데 어떻게 쓰는지 잘모르겠..
 
     public boolean register(UserDTO user) {
         if (findUserById(user.getUserId()) != null) return false;
@@ -30,6 +33,11 @@ public class UserService {
         }
         return null;
     }
+    public UserDTO findUserID(String ID){
+        return userMap.get(ID); // 여기 Map user id 조회 추가
+    }
+
+
 
     public List<UserDTO> getAllUsers() {
         return userList;
